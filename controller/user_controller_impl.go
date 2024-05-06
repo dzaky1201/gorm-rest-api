eef3ea4f9943cfec9e902914c5cc5ce7e4e1b88d
dzaky1201 <dzakyhdr@gmail.com>
2024-05-06 21:41:06 +0700

    repository,service,controller,db impl

---
+++ controller/user_controller_impl.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"belajar-rest-gorm/model"
+	"belajar-rest-gorm/model/web"
+	"belajar-rest-gorm/service"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type UserControllerImpl struct {
+	userService service.UserService
+}
+
+func NewUserController(service service.UserService) *UserControllerImpl{
+	return &UserControllerImpl{
+		userService: service,
+	}
+}
+
+func (controller *UserControllerImpl)SaveUser(c echo.Context) error  {
+	user := new(web.UserServiceRequest)
+
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, model.ResponseToClient(http.StatusBadRequest, err.Error(), nil))
+	}
+
+	if err := c.Validate(user); err != nil {
+		return err
+	}
+
+	saveUser, errSaveUser := controller.userService.SaveUser(*user)
+
+	if errSaveUser != nil {
+		return c.JSON(http.StatusBadRequest, model.ResponseToClient(http.StatusBadRequest, errSaveUser.Error(), nil))
+	}
+
+	return c.JSON(http.StatusOK, model.ResponseToClient(http.StatusOK, "berhasil membuat user", saveUser))
+}
--- service/user_service.go
+++ service/user_service.go
@@ -1,5 +1,9 @@
 package service
 
+import (
+	"belajar-rest-gorm/model/web"
+)
+
 type UserService interface {
-	SaveUser()
+	SaveUser(request web.UserServiceRequest)(map[string]interface{},error)
 }
